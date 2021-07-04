78c0857ca83c67aab697ad1aebaebf4f49507b5c
Nelson Chen <crazysim@gmail.com>
2021-07-04 02:00:38 -0700

    typical location

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
 fn main() {
-    println!("Hello, world!");
+    let log_file_location = r#"C:\Program Files (x86)\Steam\steamapps\common\Interplanetary Enhanced Edition\Interplanetary_Data\output_log.txt"#;
+    println!("{}", log_file_location);
 }
